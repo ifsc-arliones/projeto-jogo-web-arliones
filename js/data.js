a68b0003d10b3d275b4ee1c1cf4f6fbb972ae1cc
Arliones Hoeller Jr. <arliones@gmail.com>
2025-12-22 21:34:14 -0300

    Inclui jogos dos alunos.

--- js/data.js
+++ js/data.js
@@ -3,19 +3,83 @@
 
 const jogos = [
   {
-    aluno: "DeadPerquim",
+    aluno: "Daniel Bentes Perquim",
     jogo: "Space Invaders Clone",
     thumb: "assets/thumbs/DeadPerquim.png",
     url: "https://ifsc-arliones.github.io/projeto-jogo-web-DeadPerquim/",
-    descricao: "Este projeto é uma recriação do clássico jogo Space Invaders (estilo Atari 2600), desenvolvido utilizando tecnologias web modernas (HTML, CSS e JavaScript)."
+    descricao: "Este projeto é uma recriação do clássico jogo Space Invaders (estilo Atari 2600), desenvolvido utilizando tecnologias web (HTML, CSS e JavaScript)."
   },
 
   {
-    aluno: "enzobarbi",
-    jogo: "Space Invaders Clone",
-    thumb: "assets/thumbs/enzobarbi.png",
-    url: "https://ifsc-arliones.github.io/projeto-jogo-web-enzobarbi/",
-    descricao: ""
+    aluno: "Gelson Oliveira Araújo Júnior",
+    jogo: "Frogger-Style Game",
+    thumb: "assets/thumbs/Gelson-jr.png",
+    url: "https://ifsc-arliones.github.io/projeto-jogo-web-Gelson-jr/",
+    descricao: "Frogger-Style Game"
   },
+
+  {
+    aluno: "Gustavo Ribeiro",
+    jogo: "Pong",
+    thumb: "assets/thumbs/GusTav-DeV22.png",
+    url: "https://ifsc-arliones.github.io/projeto-jogo-web-GusTav-DeV22/",
+    descricao: "O jogo consiste em duas \"raquetes\" e uma bola. Cada jogador controla uma raquete e deve rebater a bola para evitar que ela ultrapasse seu lado da tela."
+  },
+
+  {
+    aluno: "Gustavo Goedert Cordova",
+    jogo: "Frogger-Style Game",
+    thumb: "assets/thumbs/gustavo-cordova.png",
+    url: "https://ifsc-arliones.github.io/projeto-jogo-web-gustavo-cordova/",
+    descricao: "Frogger-Style Game"
+  },
+
+  {
+    aluno: "Juliano Tavares da Silva",
+    jogo: "Frogger-Style Game",
+    thumb: "assets/thumbs/JulianoTavaresDaSilva.png",
+    url: "https://ifsc-arliones.github.io/projeto-jogo-web-JulianoTavaresDaSilva/",
+    descricao: "Frogger-Style Game"
+  },
+
+  {
+    aluno: "Kaleu Borges Augusto",
+    jogo: "Frogger-Style Game",
+    thumb: "assets/thumbs/kaleu-b.png",
+    url: "https://ifsc-arliones.github.io/projeto-jogo-web-kaleu-b/",
+    descricao: "Frogger-Style Game"
+  },
+
+  {
+    aluno: "Lucas Luiz Guesser",
+    jogo: "Frogger",
+    thumb: "assets/thumbs/lucasluizg.png",
+    url: "https://ifsc-arliones.github.io/projeto-jogo-web-lucasluizg/",
+    descricao: "Frogger"
+  },
+
+  {
+    aluno: "Marcos Júnior Lemes Ferreira",
+    jogo: "Frogger-Style Game",
+    thumb: "assets/thumbs/marcos-j-lemes.png",
+    url: "https://ifsc-arliones.github.io/projeto-jogo-web-marcos-j-lemes/",
+    descricao: "Desenvolvido em HTML, CSS e JavaScript, utilizando DOM e Grid para representar o mapa. O objetivo é atravessar o rio usando balsas e evitar carros até alcançar a área segura no topo."
+  },
+
+  {
+    aluno: "Nicolas Franzosi dos Santos",
+    jogo: "Space Invaders Clone",
+    thumb: "assets/thumbs/NickGamereses.png",
+    url: "https://ifsc-arliones.github.io/projeto-jogo-web-NickGamereses/",
+    descricao: "Este é um projeto de jogo estilo \"Space Invaders\" desenvolvido como parte de um trabalho acadêmico de Frontend. O jogo foi construído utilizando apenas HTML, CSS e JavaScript puro, sem frameworks externos."
+  },
+
+  // {
+  //   aluno: "enzobarbi",
+  //   jogo: "Frogger-Style Game",
+  //   thumb: "assets/thumbs/enzobarbi.png",
+  //   url: "https://ifsc-arliones.github.io/projeto-jogo-web-enzobarbi/",
+  //   descricao: ""
+  // },
 
 ];
